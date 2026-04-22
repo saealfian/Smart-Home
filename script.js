bda18743285741c579e92a09d844804a7d2718f1
Muhammad Sae Alfian <saepian04@gmail.com>
2026-04-22 15:15:44 +0700

    day 3

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ const hideMenu = () => {
 
 closeMenu.addEventListener('click', hideMenu);
 overlay.addEventListener('click', hideMenu);
+
+function togglefaq(element) {
+    const allItems = document.querySelectorAll('.faq-item');
+    const content = element.querySelector('.faq-content');
+    const img = element.querySelector('img');
+    const openIcon = '/assets/open.png';
+    const closeIcon = '/assets/close.svg';
+    const isOpening = ! content.style.maxHeight;
+
+    allItems.forEach(item => {
+        const itemContent = item.querySelector('.faq-content');
+        const itemImg = item.querySelector('img');
+        itemContent.style.maxHeight = null;
+        itemImg.src = openIcon;
+
+    });
+
+    if (isOpening) {
+        content.style.maxHeight = content.scrollHeight + 'px';
+        img.src = closeIcon;
+    }
+}
